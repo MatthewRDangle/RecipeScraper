82599ad4667f25bbce89acc97063e491a8e1d804
Matthew Dangle <matthewrdangle@gmail.com>
2025-09-22 21:52:08 -0400

    Update spec to return the full recipe

--- recipe_scraper/_scraper.py
+++ recipe_scraper/_scraper.py
@@ -23,24 +23,34 @@ class Scraper:
         """Description of the recipe."""
         raise RecipeScraperMethodNotImplemented("description")
 
+    def prep_time(self) -> int:
+        """Preparation time in minutes."""
+        raise RecipeScraperMethodNotImplemented("prep_time")
+
+    def cook_time(self) -> int:
+        """Cooking time in minutes."""
+        raise RecipeScraperMethodNotImplemented("cook_time")
+
     def instructions(self) -> list[str]:
         """Instructions on how to execute the recipe."""
         raise RecipeScraperMethodNotImplemented("instructions")
+
+    def ingredients(self) -> list[str]:
+        """Ingredients used in the recipe."""
+        raise RecipeScraperMethodNotImplemented("ingredients")
 
     def to_json(self) -> dict[str, Union[str, list[str]]]:
         """Returns a JSON representation of the recipe."""
         return {
             "title": self.title(),
             "description": self.description(),
-            "instructions": self.instructions()
+            "prep_time": self.prep_time(),
+            "cook_time": self.cook_time(),
+            "instructions": self.instructions(),
+            "ingredients": self.ingredients(),
         }
 
     def print(self) -> None:
         """Prints the recipe in JSON format."""
-        data = {
-            "title": self.title(),
-            "description": self.description(),
-            "instructions": self.instructions()
-        }
 
-        print(json.dumps(data, indent=2))
+        print(json.dumps(self.to_json(), indent=2))
--- recipe_scraper/scrapers/mccormick.py
+++ recipe_scraper/scrapers/mccormick.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from bs4 import PageElement, BeautifulSoup
+
 from recipe_scraper._scraper import Scraper
 
 class McCormickScraper(Scraper):
@@ -21,13 +25,51 @@ class McCormickScraper(Scraper):
 
         return desc
 
+    def prep_time(self) -> int:
+        local_prep_time: int = 0
+
+        info_el: BeautifulSoup | None = self.soup.find("ul", {"class": "recipe-info__list"})
+        if info_el:
+            info_li: List[PageElement] = info_el.findAll("li")
+            if info_li and info_li[0]:
+                tmp_prep_time_all_char = info_li[0].text.strip()
+                tmp_prep_time_digit_only = ''.join(char for char in tmp_prep_time_all_char if char.isdigit())
+                local_prep_time = int(tmp_prep_time_digit_only)
+
+        return local_prep_time
+
+    def cook_time(self) -> int:
+        local_cook_time: int = 0
+
+        info_el: BeautifulSoup | None = self.soup.find("ul", {"class": "recipe-info__list"})
+        if info_el:
+            info_li: List[PageElement] = info_el.findAll("li")
+            if info_li and info_li[1]:
+                tmp_cook_time_all_char = info_li[1].text.strip()
+                tmp_cook_time_digit_only = ''.join(char for char in tmp_cook_time_all_char if char.isdigit())
+                local_cook_time = int(tmp_cook_time_digit_only)
+
+        return local_cook_time
+
     def instructions(self) -> list[str]:
         instructions = []
 
         int_el = self.soup.find("div", {"class": "ff-body fs-body-60 main-recipe__text rte rte--article"})
         for el in int_el.findAll("li"):
-            text = el.get_text()
+            text = el.text.strip()
             if text:
                 instructions.append(text)
 
         return instructions
+
+    def ingredients(self) -> list[str]:
+        ingredients = []
+
+        ing_el = self.soup.find("ul", {"class": "recipe-ingredients__list"})
+        if ing_el:
+            for el in ing_el.findAll("li"):
+                text = el.text.strip()
+                if text:
+                    ingredients.append(text)
+
+        return ingredients
