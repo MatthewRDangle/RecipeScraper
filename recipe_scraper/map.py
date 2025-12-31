dc9a4573f3b22abbfe26e3ff9cc31dc8a9776fb2
Matthew Dangle <matthewrdangle@gmail.com>
2025-12-31 00:20:02 -0500

    Fix import error for map

--- recipe_scraper/map.py
+++ recipe_scraper/map.py
@@ -1,4 +1,4 @@
-from .scrapers.mccormick import McCormickScraper
+from recipe_scraper.scrapers.mccormick import McCormickScraper
 from recipe_scraper._scraper import Scraper
 
 scrapers_map: dict[str, type[Scraper]] = {
